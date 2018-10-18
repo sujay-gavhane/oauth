ad3746f40b17779d38a9bb4c5ca4f3f830c401ab
sujay-gavhane <sujay.gavhane@gmail.com>
2018-10-18 12:40:23 +0530

    Added user authentication using github

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  def self.create_with_github_login(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root to: "users#new"
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", as: :signout
+
+  resources :users do
+    member do
+      get :repositories
+    end
+  end
 end
