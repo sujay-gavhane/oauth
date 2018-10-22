4ec51b8ebfb82979d69918d79747bdf6cde47a63
sujay-gavhane <sujay.gavhane@gmail.com>
2018-10-22 10:20:24 +0530

    Changed layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def css_class_for flash_type
+	    { success: 'alert-success', error: 'alert-danger', alert: 'alert-danger', notice: 'alert-success', warning: 'alert-warning', danger: 'alert-danger' }[flash_type.to_sym] || flash_type.to_s
+	end
 end
