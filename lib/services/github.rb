7ddfc0d2873222722e2c1fba72011d4d37ce3057
sujay-gavhane <sujay.gavhane@gmail.com>
2018-10-23 18:40:20 +0530

    Fixed credentials error

--- lib/services/github.rb
+++ lib/services/github.rb
@@ -8,11 +8,11 @@ module Services
       base_uri 'https://api.github.com'
 
       def list(token, username, user_token)
-        self.class.get("/user/repos?&access_token=#{user_token}&type=all", {headers: self.headers(user_token)})        
+        self.class.get("/user/repos?&access_token=#{user_token}&type=all", {headers: self.headers(token)})        
       end
 
       def show(token, name, user_token, username, from_date, to_date)
-        self.class.get("/repos/#{username}/#{name}/commits?access_token=#{user_token}&since=#{from_date}&until=#{to_date}&", {headers: self.headers(token)})
+        self.class.get("/repos/#{username}/#{name}/commits?access_token=#{user_token}&since=#{from_date}&until=#{to_date}", {headers: self.headers(token)})
       end
     end
   end
