b31c8285e80c6ae595c176a017227d6651492e6e
sujay-gavhane <sujay.gavhane@gmail.com>
2018-10-23 17:48:40 +0530

    parsing json response

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,6 +3,7 @@ require 'services/github'
 class RepositoriesController < ApplicationController
   def index
     @responses = github_service.list(ENV['GITHUB_SECRET_TOKEN'], session[:user_name], session[:token])
+    @responses = JSON.parse(@responses.body)
   end
 
   def show
