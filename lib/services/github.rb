00d802627e44e8fb3ee1309a417f668589e5d4f7
sujay-gavhane <sujay.gavhane@gmail.com>
2018-10-23 16:53:21 +0530

    Fixed bad credentials error

--- lib/services/github.rb
+++ lib/services/github.rb
@@ -7,12 +7,12 @@ module Services
       include HTTParty
       base_uri 'https://api.github.com'
 
-      def list(token, username)
-        self.class.get("/users/#{username}/repos?&type=all", {headers: self.headers(token)})
+      def list(token, username, user_token)
+        self.class.get("/user/repos?&access_token=#{user_token}&type=all", {headers: self.headers(user_token)})        
       end
 
-      def show(token, name, username, from_date, to_date)
-        self.class.get("/repos/#{username}/#{name}/commits?&since=#{from_date}&until=#{to_date}", {headers: self.headers(token)})
+      def show(token, name, user_token, username, from_date, to_date)
+        self.class.get("/repos/#{username}/#{name}/commits?access_token=#{user_token}&since=#{from_date}&until=#{to_date}&", {headers: self.headers(token)})
       end
     end
   end
