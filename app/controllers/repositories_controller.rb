8e5602d75c72bc5b6d9da54821bb85ed98b1fc9a
sujay-gavhane <sujay.gavhane@gmail.com>
2018-11-08 01:45:57 +0530

    Added authentication

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,13 @@
 require 'services/github'
 
 class RepositoriesController < ApplicationController
+  before_action :authenticate_user
+  
   def index
-    @responses = github_service.list(ENV['GITHUB_SECRET_TOKEN'], session[:user_name], session[:token])
+    @responses = github_service.list(
+      ENV['GITHUB_SECRET_TOKEN'],
+      session[:user_name], session[:token]
+    )
     @responses = JSON.parse(@responses.body)
   end
 
@@ -12,7 +17,10 @@ class RepositoriesController < ApplicationController
   def get_repo_commits
     from_date = Date.strptime(params[:from_date], '%m/%d/%Y').strftime('%Y-%m-%d')
     to_date = Date.strptime(params[:to_date], '%m/%d/%Y').strftime('%Y-%m-%d')
-    @response = github_service.show(ENV['GITHUB_SECRET_TOKEN'], params[:id], session[:token], session[:user_name], from_date, to_date)
+    @response = github_service.show(
+      ENV['GITHUB_SECRET_TOKEN'], params[:id],
+      session[:token], session[:user_name], from_date, to_date
+    )
     @response = JSON.parse(@response.body)
   end
 
